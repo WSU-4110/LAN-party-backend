f4075df8a716b14dc6ec65e8d8d4d20ded0d76ee
Jarred Crull <gj3842@wayne.edu>
2020-10-12 17:02:13 -0400

    Changed the error responses to utilize the util

--- handlers/party.js
+++ handlers/party.js
@@ -13,10 +13,7 @@ module.exports = {
   Create: async function (event) {
       //Ensure that there was information in the event
       if(!event){
-        return {
-          statusCode: 204, //if nothing was provided in the request, return a 204 HTTPS code (No content)
-          message: JSON.stringify("No information was provided with the request"),
-        };
+        return responseUtil.Build(204, 'Event is empty');
       };
 
       //Parse the event into a request
@@ -25,27 +22,21 @@ module.exports = {
       //A party's name is valid if it has a number or letter.
       let nameChars = /'[0-9]*[A-Z]*[a-z]'/
       if (!request.name || !nameChars.test(request.name)){
-        return {
-          statusCode: 403, //Invalid name, return a 403 (Forbidden)
-          message: JSON.stringify('Party must contain a name')
-        };
+        return responseUtil.Build(403, 'Party must have a name with letters or numbers');
       };
 
       //Ensure that the party has a location
       if(!request.location){
-        return {
-          statusCode: 403, //No location, return a 403 (Forbidden)
-          message: JSON.stringify('Party must have a location')
-        };
+        return responseUtil.Build(403, 'Party must have a location');
       };
 
       //Ensure that the party has a time
       if (!request.time){
-        return {
-          statusCode: 403, //No time, return a 403 (Forbidden)
-          message: JSON.stringify('Party must have a time')
-        }
-      }
+        return responseUtil.Build(403, 'Party must have a time');
+      };
+
+      //Insert the item into the table
+      
       
 
   },
